b85050d6dc42bf0404618d3eaec6c8be888683a9
ncchen99 <ncchen99@gmail.com>
2021-10-02 11:36:25 +0800

    🐹

---
+++ collection/nckuCourse.py
@@ -0,0 +1,23 @@
+# 載入需要的套件
+import subprocess
+from selenium import webdriver
+from time import sleep
+version = subprocess.check_output(
+    ['google-chrome', '--version']).decode("utf-8").split()[2].split(".")[0]
+# 取得 chrome 板本
+driver = webdriver.Chrome("./collection/chromedriver/chromedriver" + version)
+# 開啟瀏覽器視窗(Chrome)
+driver.get("https://course.ncku.edu.tw/index.php")
+
+search_path = ["×", "中文", "課程查詢", "通識類課程", "跨域通識課程", "列表"]
+# 可能會遇到 popup 所以需要 "×"
+
+for btn_text in search_path:
+    for dom in driver.find_elements_by_xpath(
+            "//*[text()='" + btn_text + "']"):
+        try:
+            dom.click()
+            sleep(2)
+            break
+        except:
+            print("按", btn_text, "按鈕失敗")
